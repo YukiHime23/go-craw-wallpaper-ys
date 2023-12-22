3ee2ff950746445d12d4e0ffca25120c3d2cd325
kiennt1 <kiennt1@hblab.vn>
2023-12-22 11:21:41 +0700

    update

--- sqlite.go
+++ sqlite.go
@@ -12,7 +12,7 @@ var db *sql.DB
 func init() {
 	var err error
 	// Kết nối đến cơ sở dữ liệu SQLite
-	db, err = sql.Open("sqlite3", "data.db")
+	db, err = sql.Open("sqlite3", "data-azur-lane.db")
 	if err != nil {
 		log.Fatal(err)
 	}
